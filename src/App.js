d533b8c31959fcb9fc461d59e411d2b2674b4a68
github username <um_2003_8@hotmail.com>
2020-10-30 18:52:54 -0400

    add Footer component and add header and main section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Footer from './components/Footer'
 
 class App extends React.Component {
   render(){
     return(
       <div className="App">
-      
+        <header className="App-header">
+          
+        </header>
+        
+        <main>
+
+        </main>
+
+        <Footer />
       </div>
     )
   }
