cbaf5322506a96d21a60514220980c5570275bdd
github username <um_2003_8@hotmail.com>
2020-10-30 18:56:49 -0400

    create Navbar component with Navbar function

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div className= "nav">
+            <div>Uma</div>
+            <div className="nav-link">
+                <div>Home</div>
+                <div>About</div>
+                <div>Skills</div>
+                <div>Blog</div>
+                <div>Projects</div>
+                <div>Contact</div>
+            </div>
+        </div>
+
+    )
+}
+
+export default NavBar
