a49aeab4f4c533e39df0abdb901016555e1de50f
github username <um_2003_8@hotmail.com>
2020-10-30 19:40:20 -0400

    create Profile component with profile function

---
+++ src/components/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div>
+            <h3>Hi, I’m Uma Manandhar.</h3>
+            <h4>I am a Full-stack Web Developer.</h4>
+            <h5>My expertise are Ruby and JavaScript.</h5>
+        </div>
+    )
+}
+
+export default Profile
