f0768cce7f493037fbf6fae2dd38e1483f551734
github username <um_2003_8@hotmail.com>
2020-10-30 20:04:09 -0400

    add Navbar and Profile component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import Profile from './components/Profile'
 
 class App extends React.Component {
   render(){
     return(
       <div className="App">
         <header className="App-header">
-          
+          <Navbar />
         </header>
         
-        <main>
-
+        <main className= "main">
+          <Profile />
         </main>
 
         <Footer />
