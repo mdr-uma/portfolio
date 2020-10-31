d22e71ce3e5c7530205f3508b843c5671788fb6e
github username <um_2003_8@hotmail.com>
2020-10-30 20:03:22 -0400

    add className and image

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Button from './Button'
+import image from '../images/uma.JPG'
 
 const Profile = () => {
     return (
-        <div>
+        <div className="intro">
             <h3>Hi, I’m Uma Manandhar.</h3>
             <h4>I am a Full-stack Web Developer.</h4>
             <h5>My expertise are Ruby and JavaScript.</h5>
             <Button />
+            <img src={image} alt=""/>
         </div>
     )
 }
