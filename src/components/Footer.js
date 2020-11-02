535fad081f57f25a20c88c4e3a3f5e2dc08d93f1
github username <um_2003_8@hotmail.com>
2020-11-02 17:12:25 -0500

    add bootstrap padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Footer = () => {
-    return <div className="fixed-bottom text-center">Copyright © Uma’s Portfolio 2020</div>   
+    return <div className="fixed-bottom text-center p-3">Copyright © Uma’s Portfolio 2020</div>   
 }
 
 export default Footer
