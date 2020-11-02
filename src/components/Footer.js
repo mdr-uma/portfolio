0a36060dd463daf4f075e1c4ae7b1827571898a5
github username <um_2003_8@hotmail.com>
2020-11-02 00:47:52 -0500

    add fixed-bottom className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Footer = () => {
-    return <div>Copyright © Uma’s Portfolio 2020</div>   
+    return <div className="fixed-bottom text-center">Copyright © Uma’s Portfolio 2020</div>   
 }
 
 export default Footer
