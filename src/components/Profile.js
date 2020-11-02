9c020bd822a5d87c0d66c3acc698756e1f1a45b3
github username <um_2003_8@hotmail.com>
2020-11-02 16:23:50 -0500

    import icons from react-feather

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from './Button'
 import image from '../images/uma.JPG'
+import { Twitter, GitHub, Linkedin } from 'react-feather'
 
 const Profile = () => {
     return (
