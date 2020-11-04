5501a2aa13b762b1046c6d54e66ee1f420521242
github username <um_2003_8@hotmail.com>
2020-11-04 00:32:31 -0500

    create About component and class

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return(
+         <div class="wrapper">
+            <div class="row">
+                <div class="column"></div>
+                <div class="column"></div>
+            </div>
+        </div>
+    )
+}
+
+export default About
