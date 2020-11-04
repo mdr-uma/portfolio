1058a9bb1bc8f9f1e0f72665c1a9b36cbd89bbef
github username <um_2003_8@hotmail.com>
2020-11-04 00:21:48 -0500

    import Router and Route from react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import Profile from './components/Profile'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends React.Component {
