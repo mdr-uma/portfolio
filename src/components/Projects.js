77fa48bffc764563c07dd014ff39fa5cd5bbdd28
github username <um_2003_8@hotmail.com>
2020-11-05 23:47:22 -0500

    add h2 and 3 columns with className

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,18 @@ import React from 'react'
 const Projects = () => {
     return(
         <div className="container">
-
+            <h2>Projects</h2>
+            <div className="row">
+                <div className="col-sm-4 pt-3">
+                    
+                </div>
+                <div className="col-sm-4 pt-3">
+                    
+                </div>
+                <div className="col-sm-4 pt-3">
+                    
+                </div>
+            </div>
         </div>
     )
 }
