69183a19c3ea9e4743a02047cf700a16a1abd2f9
github username <um_2003_8@hotmail.com>
2020-11-05 23:33:14 -0500

    add icons and break line after h2

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,21 +3,24 @@ import React from 'react'
 const Skills = () => {
     return(
         <div className="container">
-            <h2>Skills Overview</h2>
+            <h2>Skills Overview</h2><br/>
             <div className="row">
                 <div className="col-sm-4 pt-3">
+                    <i className="fas fa-folder-plus icons"></i>
                     <h4>Front-end</h4>
                     <p>- React/Redux</p> 
                     <p>- Javascript</p>
                     <p>- HTML/CSS</p>
                 </div>
                 <div className="col-sm-4 pt-3">
+                    <i className="fas fa-database icons"></i>
                     <h4>Back-end</h4>
                     <p>- Ruby</p>
                     <p>- Ruby on Rails</p>
                     <p>- SQLite/Postgresql</p>
                 </div>
                 <div className="col-sm-4 pt-3">
+                    <i className="fas fa-toolbox icons"></i>
                     <h4>Other tools</h4>
                     <p>- Sinatra</p>
                     <p>- Figma/Adobe Photoshop</p>
