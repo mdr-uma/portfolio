6a55ebde02d661ac4c4a30f35ef28abadbfbd4b0
github username <um_2003_8@hotmail.com>
2020-11-05 23:10:55 -0500

    add skills in p tag

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,12 +7,21 @@ const Skills = () => {
             <div className="row">
                 <div className="col-sm-4 pt-3">
                     <h4>Front-end</h4>
+                    <p>- React/Redux</p> 
+                    <p>- Javascript</p>
+                    <p>- HTML/CSS</p>
                 </div>
                 <div className="col-sm-4 pt-3">
                     <h4>Back-end</h4>
+                    <p>- Ruby</p>
+                    <p>- Ruby on Rails</p>
+                    <p>- SQLite/Postgresql</p>
                 </div>
                 <div className="col-sm-4 pt-3">
                     <h4>Other tools</h4>
+                    <p>- Sinatra</p>
+                    <p>- Figma/Adobe Photoshop</p>
+                    <p>- Bootstrap/Github</p>
                 </div>
             </div>
         </div>
