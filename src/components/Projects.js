209431f27b459c65662b594fc7657110620dd296
github username <um_2003_8@hotmail.com>
2020-11-05 23:53:38 -0500

    add h4 tag to each column div

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,12 +6,15 @@ const Projects = () => {
             <h2>Projects</h2>
             <div className="row">
                 <div className="col-sm-4 pt-3">
+                    <h4>Remote Jobs</h4>
+
+                </div>
+                <div className="col-sm-4 pt-3">
+                    <h4>Bites for Tikes</h4>
                     
                 </div>
                 <div className="col-sm-4 pt-3">
-                    
-                </div>
-                <div className="col-sm-4 pt-3">
+                    <h4>Positive Quotes</h4>
                     
                 </div>
             </div>
