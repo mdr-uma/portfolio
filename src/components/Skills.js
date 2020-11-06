a5db13fdd5338d91c7a29b14cba8be18033918c3
github username <um_2003_8@hotmail.com>
2020-11-05 22:50:22 -0500

    add h2, columns and classNames

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Skills = () => {
     return(
-        <div>
-
+        <div className="container">
+            <h2>Skills Overview</h2>
+            <div className="row">
+                <div className="col-sm-4 pt-3">Front-end</div>
+                <div className="col-sm-4 pt-3">Back-end</div>
+                <div className="col-sm-4 pt-3">Other tools</div>
+            </div>
         </div>
     )
 }
