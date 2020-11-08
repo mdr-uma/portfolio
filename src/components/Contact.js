5ecfcf053a905608bcf59c305f1b6f57a7b91b45
github username <um_2003_8@hotmail.com>
2020-11-08 00:03:48 -0500

    create Contact function with h2 tag and className

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div className="container">
+            <h2>Contact Info</h2>
+        </div>
+
+    )
+}
+
+export default Contact 
