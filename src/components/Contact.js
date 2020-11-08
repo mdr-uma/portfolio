9e5db17b003adc20c08c4a9e29042d5e05b39a0b
github username <um_2003_8@hotmail.com>
2020-11-08 00:09:04 -0500

    add contact icons from font awesome

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,9 +6,9 @@ const Contact = () => {
             <h2>Contact Info</h2>
             <div className="h-100 d-flex justify-content-center align-items-center">
                 <div className="col-sm-4 p-5 text-left">
-                    <h5>Boston, Massachusetts</h5><hr/>
-                    <h5>mdr.uma28@gmail.com</h5><hr/>
-                    <h5>617.309.0676</h5>
+                    <h5><i className="fas fa-map-marker-alt contact-icon"></i>Boston, Massachusetts</h5><hr/>
+                    <h5><i className="fas fa-envelope-square contact-icon"></i> mdr.uma28@gmail.com</h5><hr />
+                    <h5><i className="fas fa-phone-alt contact-icon"></i>617.309.0676</h5>
                 </div>
             </div>
         </div>
