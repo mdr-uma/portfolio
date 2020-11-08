75c719aedcc829a3414a50e4ba29fcfca391448e
github username <um_2003_8@hotmail.com>
2020-11-08 00:08:02 -0500

    add contact info with bootstrap div classes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,13 @@ const Contact = () => {
     return(
         <div className="container">
             <h2>Contact Info</h2>
+            <div className="h-100 d-flex justify-content-center align-items-center">
+                <div className="col-sm-4 p-5 text-left">
+                    <h5>Boston, Massachusetts</h5><hr/>
+                    <h5>mdr.uma28@gmail.com</h5><hr/>
+                    <h5>617.309.0676</h5>
+                </div>
+            </div>
         </div>
 
     )
