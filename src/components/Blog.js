223f345159c820c9a80cc79e93693df425208fe1
github username <um_2003_8@hotmail.com>
2020-11-08 23:25:13 -0500

    add h2 and 3 column divs

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,7 +3,18 @@ import React from 'react'
 const Blog = () => {
     return(
         <div className="container">
+            <h2>Blog</h2>
+            <div className="row">
+                <div className="col-sm-4 pt-3">
+                    <h4>What I Know About Redux So Far</h4>
+                </div>
+                <div className="col-sm-4 pt-3">
 
+                </div>
+                <div className="col-sm-4 pt-3">
+                    
+                </div>
+            </div>
         </div>
     )
 }
