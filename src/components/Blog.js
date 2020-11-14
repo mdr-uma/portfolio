ce950776de3d5e2b3e5de992584194343be642e1
github username <um_2003_8@hotmail.com>
2020-11-13 23:10:26 -0500

    add shadow

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -9,7 +9,7 @@ const Blog = () => {
                 <div className="col-sm-6 pt-3">
                     <h4>React/Redux</h4>
                     <a href="https://dev.to/uma/how-i-implemented-redux-in-my-react-project-3c79">
-                        <img width="360px" height="240px" src={image} alt="pic" />
+                        <img width="360px" height="240px" src={image} alt="pic" className="shadow-lg"/>
                     </a>
                 </div>
                 <div className="col-sm-6 pt-3">
