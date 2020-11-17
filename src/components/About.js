d3e92cb6380d0f26e2fe78ad3846b4987631e206
github username <um_2003_8@hotmail.com>
2020-11-17 00:25:06 -0500

    add break

--- src/components/About.js
+++ src/components/About.js
@@ -7,14 +7,14 @@ const About = () => {
             <div className="row">
                 <div className="col-sm-6">
 
-                I’m a Fullstack Web Developer. But growing up in a small country like Nepal, where education for girls is not even considered, I was fortunate to have a chance to go to school and learn basic computer knowledge during high school.
+                I’m a Fullstack Web Developer. But growing up in a small country like Nepal, where education for girls is not even considered, I was fortunate to have a chance to go to school and learn basic computer knowledge during high school. <br/><br/>
 
-                I was excited, curious, fascinated by every little thing I was learning. It was another world for me. All of a sudden, I felt like I got this power to see other parts of the world, and I could connect with people from all over the world. That's when I decided I want to learn more but, as I mentioned, it wasn’t easy for girls to step out and do what they wanted.
+                I was excited, curious, fascinated by every little thing I was learning. It was another world for me. All of a sudden, I felt like I got this power to see other parts of the world, and I could connect with people from all over the world. That's when I decided I want to learn more but, as I mentioned, it wasn’t easy for girls to step out and do what they wanted.<br/><br/>
 
                 After lots of struggles, I took a Graphic Design course and started working as a Graphic Designer plus started learning coding with my friend.
 
                 </div>
-                <div className="col-sm-6">I chose different career paths , where I learned the fine art of customer service and teaching. But deep down, I always wanted to be in the technology field. I never gave up, which is why even after so many years, I joined a coding bootcamp to fulfill my dream.
+                <div className="col-sm-6">I chose different career paths , where I learned the fine art of customer service and teaching. But deep down, I always wanted to be in the technology field. I never gave up, which is why even after so many years, I joined a coding bootcamp to fulfill my dream.<br/><br/>
 
                 No matter where I work, I bring the same dedication and passion I have always had. As a multilingual expat, I adapt to different environments and cultures. I always want to learn, so I’m always ready for challenges.</div>
             </div>
