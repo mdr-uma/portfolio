c19ea7d14ea86e11b63531b194207feb9b170263
github username <um_2003_8@hotmail.com>
2020-11-19 01:10:46 -0500

    add break and move h4 below break

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,16 +7,19 @@ const Projects = () => {
             <h2>Projects</h2>
             <div className="row">
                 <div className="col-sm-4 pt-3 mr-.5">
-                    <h4>Remote Jobs</h4>
                     <ReactPlayer width="360px" height="240px" controls url="https://www.youtube.com/watch?v=rsTp_xUZNXg&ab_channel=umamanandhar"/>
+                    <br/>
+                    <h4>Remote Jobs</h4>
                 </div>
                 <div className="col-sm-4 pt-3 mr-.5">
-                    <h4>Bites for Tikes</h4>
                     <ReactPlayer width="360px" height="240px" controls url="https://www.youtube.com/watch?v=YrHOMUZceGY&ab_channel=umamanandhar" />
+                    <br/>
+                    <h4>Bites for Tikes</h4>
                 </div>
                 <div className="col-sm-4 pt-3 mr-.5">
-                    <h4>Positive Quotes</h4>
                     <ReactPlayer width="360px" height="240px" controls url="https://www.youtube.com/watch?v=sBPXnwbElYo&ab_channel=umamanandhar" />     
+                    <br/>
+                    <h4>Positive Quotes</h4>
                 </div>
             </div>
         </div>
