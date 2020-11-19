ca64840d5b5b15d41748b1b75f28bd48eb9ce59a
github username <um_2003_8@hotmail.com>
2020-11-18 23:29:56 -0500

    add className

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
         <div className="container">
             <h2>Contact Info</h2>
             <div className="h-100 d-flex justify-content-center align-items-center">
-                <div className="col-sm-4 p-5 text-left">
+                <div className="skill col-sm-4 p-5 shadow text-left">
                     <h5><i className="fas fa-map-marker-alt contact-icon"></i>Boston, Massachusetts</h5><hr/>
                     <h5><i className="fas fa-envelope-square contact-icon"></i> mdr.uma28@gmail.com</h5><hr />
                     <h5><i className="fas fa-phone-alt contact-icon"></i>617.309.0676</h5>
