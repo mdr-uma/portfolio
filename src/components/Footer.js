6d527641e024f05d2dd4a271623a8c5d5b583ca7
github username <um_2003_8@hotmail.com>
2020-11-22 00:30:59 -0500

    add margin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
-    return <div className="fixed-bottom text-center p-3">Copyright © Uma’s Portfolio 2020</div>   
+    return(
+        <div className="fixed-bottom text-center pt-3 alert alert-danger" role="alert" style={{marginBottom: 0}}>
+            Copyright © Uma’s Portfolio 2020
+        </div>   
+    )
 }
 
 export default Footer
