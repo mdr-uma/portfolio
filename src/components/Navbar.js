8bb7699aec69009436a46fdb5fbe9aad9f6bcebd
github username <um_2003_8@hotmail.com>
2020-11-23 00:20:01 -0500

    switch logo.png with LOGO.jpg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import image from '../images/logo.png'
+import image from '../images/LOGO.jpg'
 
 const NavBar = () => {
     return (
