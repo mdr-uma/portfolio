e4d5056997f5747e554a061201d88a3a8dd9400c
github username <um_2003_8@hotmail.com>
2020-11-23 00:25:14 -0500

    add navbar links components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import About from './About'
+import Blog from './Blog'
+import Contact from './Contact'
 import Profile from './Profile'
+import Projects from './Projects'
+import Skills from './Skills'
 
 const Home = () => {
     return (
-        <div>
-            <Profile />
+        <div className="container">
+            <div className="pb-5 mb-5">
+                <Profile />
+            </div>
+            <div className="pb-5">
+                <About />
+            </div>
+            <div className="pb-5 mb-5">
+                <Skills />
+            </div>
+            <div className="pb-5 mb-5">
+                <Projects />
+            </div>
+            <div className="pb-5 mb-5">
+                <Blog />
+            </div>
+            <div className="pb-5 mb-5">
+                <Contact />
+            </div>
         </div>
     )
 }
