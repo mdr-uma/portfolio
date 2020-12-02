828475465772bc9213ce3a60242fd57534c1d1be
github username <um_2003_8@hotmail.com>
2020-12-01 21:25:59 -0500

    add blog post links and images

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import image from '../images/redux.png'
+import image1 from '../images/higher-order.png'
 
 const Blog = () => {
     return(
         <div className="container">
+            <br/>
             <h2>Blog</h2>
             <div className="row">
                 <div className="col-sm-4 pt-3">
@@ -13,10 +15,16 @@ const Blog = () => {
                     </a>
                 </div>
                 <div className="col-sm-4 pt-3">
-
+                    <h4>React/Redux</h4>
+                    <a href="https://dev.to/uma/higher-order-components-5e52">
+                        <img width="360px" height="240px" src={image1} alt="pic" className="shadow-lg" />
+                    </a>
                 </div>
                 <div className="col-sm-4 pt-3">
-
+                    <h4>React/Redux</h4>
+                    <a href="https://dev.to/uma/how-i-implemented-redux-in-my-react-project-3c79">
+                        <img width="360px" height="240px" src={image} alt="pic" className="shadow-lg" />
+                    </a>
                 </div>
             </div>
         </div>
