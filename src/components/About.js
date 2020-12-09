cf92a508b52ae5ce74b6db655804a14699daaf08
github username <um_2003_8@hotmail.com>
2020-12-08 22:29:04 -0500

    remove break

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,6 @@ import React from 'react'
 const About = () => {
     return(
         <div className="container">
-            <br/>
             <h2>About Me</h2>
             <div className="row">
                 <div className="col-sm-6">
