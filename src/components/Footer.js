de20f3c5d1f01efb6aa0a217b7df093bdf343dba
github username <um_2003_8@hotmail.com>
2020-12-16 17:58:27 -0500

    update footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return(
         <div className="fixed-bottom text-center pt-3 alert alert-danger" role="alert" style={{marginBottom: 0}}>
-            Copyright © Uma’s Portfolio 2020
+            Create and Design by Uma
         </div>   
     )
 }
