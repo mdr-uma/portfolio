cccb56cbf50938b71cd9d6e548ea503927147f76
github username <um_2003_8@hotmail.com>
2020-12-23 22:10:38 -0500

    fix typo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return(
         <div className="fixed-bottom text-center pt-3 alert alert-danger" role="alert" style={{marginBottom: 0}}>
-            Create and Design by Uma
+            Created and Designed by Uma
         </div>   
     )
 }
