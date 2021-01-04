2bbbbcfbd0c8fb45a786b0dfdf41f0d8c6d318c3
github username <um_2003_8@hotmail.com>
2021-01-04 18:32:18 -0500

    add bootstrap class carousel to slide effect

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -8,24 +8,30 @@ const Blog = () => {
         <div className="container">
             <br/>
             <h2>Blog</h2>
-            <div className="row">
-                <div className="col-sm-4 pt-3">
-                    <h4>React/Redux</h4>
-                    <a href="https://dev.to/uma/how-i-implemented-redux-in-my-react-project-3c79">
-                        <img width="360px" height="240px" src={image} alt="pic" className="shadow-lg"/>
-                    </a>
-                </div>
-                <div className="col-sm-4 pt-3">
-                    <h4>React</h4>
-                    <a href="https://dev.to/uma/higher-order-components-5e52">
-                        <img width="360px" height="240px" src={image1} alt="pic" className="shadow-lg" />
-                    </a>
-                </div>
-                <div className="col-sm-4 pt-3">
-                    <h4>React</h4>
-                    <a href="https://dev.to/uma/state-v-s-props-3pn0">
-                        <img width="360px" height="240px" src={image2} alt="pic" className="shadow-lg" />
-                    </a>
+            <div id="demo" className="carousel" data-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <div className="row">
+                            <div className="col-sm-4 pt-3">
+                                <h4>React/Redux</h4>
+                                <a href="https://dev.to/uma/how-i-implemented-redux-in-my-react-project-3c79">
+                                    <img width="360px" height="240px" src={image} alt="pic" className="shadow-lg"/>
+                                </a>
+                            </div>
+                            <div className="col-sm-4 pt-3">
+                                <h4>React</h4>
+                                <a href="https://dev.to/uma/higher-order-components-5e52">
+                                    <img width="360px" height="240px" src={image1} alt="pic" className="shadow-lg" />
+                                </a>
+                            </div>
+                            <div className="col-sm-4 pt-3">
+                                <h4>React</h4>
+                                <a href="https://dev.to/uma/state-v-s-props-3pn0">
+                                    <img width="360px" height="240px" src={image2} alt="pic" className="shadow-lg" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
