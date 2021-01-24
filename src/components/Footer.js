0d6cbec0d1176f89a6233e3438eb0e2685e7541c
github username <um_2003_8@hotmail.com>
2021-01-23 23:30:18 -0500

    switch inline style with bootstrap style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return(
-        <div className="fixed-bottom text-center pt-3 alert alert-danger" role="alert" style={{marginBottom: 0}}>
+        <div className="fixed-bottom text-center pt-3 mb-0 alert alert-danger" role="alert">
             Created and Designed by Uma
         </div>   
     )
