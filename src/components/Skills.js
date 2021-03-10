dc90b654e8a8db249f5213a8406cf360e66e2e4d
github username <um_2003_8@hotmail.com>
2021-03-10 00:36:35 -0500

    add little room between skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,21 +6,21 @@ const Skills = () => {
             <br/>
             <h2>Skills Overview</h2><br/>
             <div className="row">
-                <div className="skill col-md-4 pt-3 shadow mb-5 mb-sm-5">
+                <div className="skill col-md-4 pt-3 mb-5 mb-sm-5">
                     <i className="fas fa-folder-plus icons"></i>
                     <h4>Front-end</h4>
                     <p>- React/Redux</p> 
                     <p>- Javascript</p>
                     <p>- HTML/CSS</p>
                 </div>
-                <div className="skill col-md-4 pt-3 shadow mb-5 mb-sm-5">
+                <div className="skill col-md-4 pt-3 mb-5 mb-sm-5">
                     <i className="fas fa-database icons"></i>
                     <h4>Back-end</h4>
                     <p>- Ruby</p>
                     <p>- Ruby on Rails</p>
                     <p>- SQLite/Postgresql</p>
                 </div>
-                <div className="skill col-md-4 pt-3 shadow mb-5 mb-sm-5">
+                <div className="skill col-md-4 pt-3 mb-5 mb-sm-5">
                     <i className="fas fa-toolbox icons"></i>
                     <h4>Other tools</h4>
                     <p>- Sinatra</p>
