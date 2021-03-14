2ff0834b09f82a7a6c34513bccb7e67bf834b74a
github username <um_2003_8@hotmail.com>
2021-03-14 14:36:53 -0400

    add smooth scrolling feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,42 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import image from '../images/LOGO.jpg'
+import { Link, animateScroll as scroll } from 'react-scroll'
+import image from '../images/portfolio-logo.png'
 
 const NavBar = () => {
     return (
-        <div className="container">
-            <nav className="navbar navbar-expand-sm navbar-light bg-transparent d-flex justify-content-end">
-                <Link className="btn btn-success" to="/">Home</Link>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <div className="container d-flex justify-content-space-between">
+                <div>
+                    <Link to="/" onClick={() => scroll.scrollToTop()}>
+                        <img src={image} alt="logo" className="logo animate__animated animate__tada"/>
+                    </Link>
+                </div>
+
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
                     <span className="navbar-toggler-icon"></span>
                 </button>
+                
                 <div className="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
                     <ul className="navbar-nav text-center">  
                         <li className="nav-item">
-                            <Link className="nav-link" to="/about">About</Link>
+                            <Link className="nav-link" to="about" smooth={true} duration={1000}>About</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/skills">Skills</Link>
+                            <Link className="nav-link" to="skills" smooth={true} duration={1000}>Skills</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/projects">Projects</Link>
+                            <Link className="nav-link" to="projects" smooth={true} duration={1000}>Projects</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/blog">Blog</Link>
+                            <Link className="nav-link" to="blog" smooth={true} duration={1000}>Blog</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/contact">Contact</Link>
+                            <Link className="nav-link" to="contact" smooth={true} duration={1000}>Contact</Link>
                         </li>
                     </ul>
                 </div>
-            </nav>
-            <a href="/"><img src={image} alt="logo" className="logo animate__animated animate__tada"/></a>
-        </div>
+            </div>
+        </nav>
     )
 }
 
