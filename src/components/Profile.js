7cc564b0685e2f8145ea304acf58b2f5d97bbd8d
github username <um_2003_8@hotmail.com>
2021-03-24 00:16:05 -0400

    break lines within a tag for readability

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -36,9 +36,15 @@ const Profile = () => {
                 <div className="col-md-6">
                     <div className="intro">
                         <img src={image} alt="" className="shadow"/>
-                        <a href="https://twitter.com/imoohma"><i className="fab fa-twitter-square shadow animate__animated animate__bounceInDown" id="twitter"></i></a>   
-                        <a href=" https://github.com/mdr-uma"><i className="fab fa-github-square shadow animate__animated animate__bounceInDown" id="git"></i></a>
-                        <a href="https://www.linkedin.com/in/uma-manandhar"><i className="fab fa-linkedin shadow animate__animated animate__bounceInDown" id="linkedin"></i></a>
+                        <a href="https://twitter.com/imoohma">
+                            <i className="fab fa-twitter-square shadow animate__animated animate__bounceInDown" id="twitter"></i>
+                        </a>   
+                        <a href=" https://github.com/mdr-uma">
+                            <i className="fab fa-github-square shadow animate__animated animate__bounceInDown" id="git"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/uma-manandhar">
+                            <i className="fab fa-linkedin shadow animate__animated animate__bounceInDown" id="linkedin"></i>
+                        </a>
                     </div >
                     <div className="img-background shadow">
                         <video autoPlay loop muted className="bg-video">
